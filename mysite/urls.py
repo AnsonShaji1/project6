a3b66950050a4698115cbd72411134877c25879a
AnsonShaji1 <ansonshaji8@gmail.com>
2017-01-30 15:10:46 +0530

    second commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^sale_app/',include('sale_app.urls')),
+    url(r'',include('sale_app.urls')),
 ]
 
 
--- sale_app/urls.py
+++ sale_app/urls.py
@@ -4,11 +4,11 @@ from django.contrib.auth.views import login,logout
 
 urlpatterns=[
     url(r'^$',views.post_list,name='post_list'),
-    url(r'^(?P<pk>\d+)/$',views.post_detail,name='post_detail'),
-    url(r'^new/$',views.post_new,name='post_new'),
-    url(r'^(?P<pk>\d+)/edit/$',views.post_edit,name='post_edit'),
-    url(r'^login/$',login,{'template_name':'sale_app/login.html'}),
-    url(r'^logout/$',logout,{'template_name':'sale_app/logout.html'}),
-    url(r'^register/$',views.register,name='register'),
+    url(r'^sale_app/(?P<pk>\d+)/$',views.post_detail,name='post_detail'),
+    url(r'^sale_app/new/$',views.post_new,name='post_new'),
+    url(r'^sale_app/(?P<pk>\d+)/edit/$',views.post_edit,name='post_edit'),
+    url(r'^sale_app/login/$',login,{'template_name':'sale_app/login.html'}),
+    url(r'^sale_app/logout/$',logout,{'template_name':'sale_app/logout.html'}),
+    url(r'^sale_app/register/$',views.register,name='register'),
 
 ]
